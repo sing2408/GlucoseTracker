f6d750f4d5007da86d6fff56882bd06721788c90
Singgih Tulus Makmud <133790648+sing2408@users.noreply.github.com>
2024-07-10 17:47:11 +0700

    Inital Commit
    Inital Commit

---
+++ GlucoseTracker/GlucoseTracker/GlucoseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlucoseTrackerApp.swift
+//  GlucoseTracker
+//
+//  Created by Singgih Tulus Makmud on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GlucoseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
