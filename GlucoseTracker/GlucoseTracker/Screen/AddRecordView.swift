2d6322540b71d2da04efe612cf5536cddfa360d2
Rio Ikhsan <mrikhsankamal@gmail.com>
2024-07-11 11:26:43 +0700

    initial commit. add inital form add new record

--- GlucoseTracker/GlucoseTracker/Screen/AddRecordView.swift
+++ GlucoseTracker/GlucoseTracker/Screen/AddRecordView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct AddRecordView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @Environment(\.modelContext) var context
+
+    @State var date: Date = Date()
+    @State var amount: String = ""
+    @State var type: String = ""
+    @State var notes: String = ""
+    
     var body: some View {
-      Text("Ini add record view")
+        Form{
+            Section{
+                TextField("Glucose Level", text: $amount)
+                    .keyboardType(.numberPad)
+                DatePicker("Date", selection: $date, displayedComponents: .date)
+                Picker(selection: $type) {
+                    Text("Before Meal")
+                    Text("After Meal")
+                } label: {
+                    Text("Type")
+                }
+                TextField("Note", text: $notes)
+
+            }
+            Button(action: {
+                saveNewRecord()
+                dismiss()
+            }, label: {
+                Text("Save")
+                    .frame(maxWidth: .infinity)
+            })
+        }
     }
+    func saveNewRecord(){
+        guard let amountInt = Int(amount) else {
+            // Handle invalid input
+            return
+        }
+        
+        let newRecord = GlucoseData( date: date, amount: amountInt, type: type, notes: notes)
+        context.insert(newRecord)
+        dismiss()
+    }
+
 }
 
 #Preview {
