f43ed8544db92602e8245970cbe92daaf61d1cf3
Singgih Tulus Makmud <133790648+sing2408@users.noreply.github.com>
2024-07-11 23:04:03 +0700

    Create home page (lofi)

--- GlucoseTracker/GlucoseTracker/ViewModel/HomeViewModel.swift
+++ GlucoseTracker/GlucoseTracker/ViewModel/HomeViewModel.swift
@@ -5,4 +5,28 @@
 //  Created by Singgih Tulus Makmud on 11/07/24.
 //
 
-import Foundation
+import SwiftUI
+
+class HomeViewModel: ObservableObject {
+    @Published var startDateString: String
+    @Published var endDateString: String
+    @Published var isShowingSheet = false
+    
+    init() {
+        let calendar = Calendar.current
+        let now = Date()
+        
+        // Calculate the start and end dates
+        let endDate = calendar.startOfDay(for: now)
+        let startDate = calendar.date(byAdding: .day, value: -6, to: endDate)!
+        
+        // Create the date range strings using the custom DateFormatter
+        self.startDateString = DateFormatter.custom.string(from: startDate)
+        self.endDateString = DateFormatter.custom.string(from: endDate)
+    }
+    
+    func toggleSheet() {
+        isShowingSheet.toggle()
+    }
+}
+
